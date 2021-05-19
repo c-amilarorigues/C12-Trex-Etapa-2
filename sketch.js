8270b8ee52f8cc2466a03da13d6a3ed382d4e0fc
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-05-18 23:03:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,30 +21,30 @@ function setup() {
 
   createCanvas(600,200)
   
-  //create a trex sprite
+  //crie um sprite de trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
   trex.scale = 0.5;
   
-  //create a ground sprite
+  //crie sprite ground (solo)
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
   
-  //creating invisible ground
+  //crie um solo invisível
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
  
 }
 
 function draw() {
-  //set background color
+  //definir cor do plano de fundo
   background(220);
   
   
   
-  // jump when the space key is pressed
+  // pulando o trex ao pressionar a tecla de espaço
   if(keyDown("space")&& trex.y >= 100) {
     trex.velocityY = -10;
   }
@@ -55,7 +55,7 @@ function draw() {
     ground.x = ground.width/2;
   }
   
-  //stop trex from falling down
+  //impedir que o trex caia
   trex.collide(invisibleGround);
 
   
